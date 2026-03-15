6d1b1c8c595b239c3285ea7d1bdcdfc0ccf22245
Matt Craddock <matt@mattcraddock.com>
2026-03-15 14:48:13 +0000

    keep copy of original matrix for pca

--- R/infomax.R
+++ R/infomax.R
@@ -94,6 +94,7 @@ run_infomax <- function(x,
   }
 
   # Perform PCA if specified
+  x_orig <- x
   pca_decomp <- if (!is.null(pca)) {
     pca_decomp <- eigen(stats::cov(x))
     x <- x %*% pca_decomp$vectors[, 1:pca]
@@ -140,7 +141,7 @@ run_infomax <- function(x,
     message(sprintf("ICA running time: %.3f s", end_time[[3]]))
   }
 
-  S <- x %*% unmixing_mat
+  S <- x_orig %*% unmixing_mat
   colnames(S) <- sprintf("Comp%03d", 1:ncol(S))
 
   list(M = mixing_mat, W = unmixing_mat, S = S, iter = rotation_mat$iter)
